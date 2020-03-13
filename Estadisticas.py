c5ce0eda73eca393e7d61f1a6dedf930a4aa37c2
jfmatheusg <jfmatheusg@gmail.com>
2020-03-12 20:12:14 -0500

    Iteración 3: Devolver un arreglo con el número de elementos, el mínimo y el máximo- Paso 4: Se modifica codigo para n elementos en la cadena (verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -15,9 +15,6 @@ class Estadisticas:
                     min = num
                 if num > max:
                     max = num
-            if tam > 3:
-                return tam, int(min)
-            else:
-                return tam, int(min), int(max)
+            return tam, int(min), int(max)
         else:
             return 1, int(cadena), int(cadena)
