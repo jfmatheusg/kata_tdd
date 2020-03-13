e344b3c80bddbd10520dfdb6fb6e06f61a0770a3
jfmatheusg <jfmatheusg@gmail.com>
2020-03-12 20:04:56 -0500

    Iteración 3: Devolver un arreglo con el número de elementos, el mínimo y el máximo- Paso 3: Se agrega prueba para 2 elemento en la cadena (rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -10,7 +10,7 @@ class EstadisticasTest(TestCase):
         self.assertEqual(Estadisticas.getEstadisticas("1"), (1, 1, 1), "Cadena un elemento")
 
     def test_cadenadeDosElementos(self):
-        self.assertEqual(Estadisticas.getEstadisticas("5,4"), (2, 4), "Cadena dos elementos")
+        self.assertEqual(Estadisticas.getEstadisticas("5,4"), (2, 4, 5), "Cadena dos elementos")
 
     def test_cadenadeNElementos(self):
         self.assertEqual(Estadisticas.getEstadisticas("3,2,8,5"), (4, 2), "Cadena de 4 elementos (n)")
