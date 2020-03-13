9fa0f84446fe42fda79fda751e48fed2c51b6633
Magda Mejía <magda.mejia96@gmail.com>
2020-03-12 20:23:37 -0500

    Iteración 4 Devolver un arreglo con el número de elementos, el mínimo, máximo y el promedio, Paso 3 Se modifica código para cadena de 2 elementos (Verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -9,12 +9,17 @@ class Estadisticas:
             tam = 0
             min = num[0]
             max = num[0]
+            suma = 0;
             for num in num:
                 tam += 1
+                suma += int(num)
                 if num < min:
                     min = num
                 if num > max:
                     max = num
-            return tam, int(min), int(max)
+            if tam > 3:
+                return tam, int(min), int(max)
+            else:
+                return tam, int(min), int(max), suma / tam
         else:
             return 1, int(cadena), int(cadena), int(cadena)
