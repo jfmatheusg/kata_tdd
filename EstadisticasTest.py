6875be31fe1688451d0e2f9cbe3d78be7a0611cd
Magda Mejía <magda.mejia96@gmail.com>
2020-03-12 19:18:26 -0500

    Iteración 1 Devolver un arreglo con el número de elementos, Paso 2 Se agrega prueba para cadena de un número (Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -5,3 +5,6 @@ from Estadisticas import Estadisticas
 class MaximoTest(TestCase):
     def test_maximocadenavacia(self):
         self.assertEqual(Estadisticas.getEstadisticas(""), 0, "Cadena vacia")
+
+    def test_maximocadeUnElemento(self):
+        self.assertEqual(Estadisticas.getEstadisticas("1"), 1, "Cadena un elemento")
