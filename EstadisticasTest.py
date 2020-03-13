05fb10b0e72b9f8d2cc8fe9e109e4c574d9ed00e
Magda Mejía <magda.mejia96@gmail.com>
2020-03-12 19:54:42 -0500

    Iteración 2 Devolver un arreglo con el número de elementos y el mínimo, Paso 4 Se agrega prueba para cadena de n elementos (Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -13,4 +13,4 @@ class EstadisticasTest(TestCase):
         self.assertEqual(Estadisticas.getEstadisticas("5,4"), (2,4), "Cadena dos elementos")
 
     def test_cadenadeNElementos(self):
-        self.assertEqual(Estadisticas.getEstadisticas("3,2,8,5"), 4, "Cadena de 4 elementos (n)")
+        self.assertEqual(Estadisticas.getEstadisticas("3,2,8,5"), (4,2), "Cadena de 4 elementos (n)")
