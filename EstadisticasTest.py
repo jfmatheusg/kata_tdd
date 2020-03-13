4373288acf0cd69ae06266954355093e0914698d
jfmatheusg <jfmatheusg@gmail.com>
2020-03-12 19:36:04 -0500

    Iteración 2: Devolver un arreglo con el número de elementos y el minimo- Paso 1: Se agrega prueba para arreglo vacio (rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -4,7 +4,7 @@ from Estadisticas import Estadisticas
 
 class EstadisticasTest(TestCase):
     def test_cadenaVacia(self):
-        self.assertEqual(Estadisticas.getEstadisticas(""), 0, "Cadena vacia")
+        self.assertEqual(Estadisticas.getEstadisticas(""), (0,0), "Cadena vacia")
 
     def test_cadenadeUnElemento(self):
         self.assertEqual(Estadisticas.getEstadisticas("1"), 1, "Cadena un elemento")
