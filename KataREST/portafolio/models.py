66242520e9a9e5d0314e17e8f76260fedf6e1fb4
jfmatheusg <jfmatheusg@gmail.com>
2020-03-28 10:57:41 -0500

    linea base Kata REST

---
+++ KataREST/portafolio/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000, null=True)
+    type = models.CharField(max_length=5, blank=True)
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, null=True)
+    image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
